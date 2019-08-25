c40c538fe7fcd81df95b57b4a69bbd870cf2ee62
Henrique S. Hinke <hhinke@gmail.com>
2019-08-25 00:29:59 -0300

    First commit of draft version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+export default function App() {
+  const [bestMatches, setBestMatches] = useState(null);
+
+  async function loadSearch(event) {
+    const API_KEY = '<YOUR_APIKEY_HERE>';
+    const searchAsset = event.target.value;
+    if(searchAsset.length < 4) {
+      setBestMatches(null);
+      return;
+    }
+
+    const response = await axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchAsset}&apikey=${API_KEY}`);
+
+    const { bestMatches } = response.data;
+    const filteredBestMatches = bestMatches.filter((match, index) => index < 3);
+    setBestMatches(filteredBestMatches);
+
+    console.log(filteredBestMatches);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <input id='assetName' 
+            type='text' 
+            placeholder='Asset Name' 
+            onChange={e => loadSearch(e)}>              
+      </input>
+      { bestMatches != null ? (
+        <div className="results-container">
+          <ul className="results-list">
+            { bestMatches.map(match => (
+              <li key={match['1. symbol']}>
+                <h3>{match['1. symbol']}</h3>
+                <p>{match['2. name']}</p>
+              </li> 
+            )) }
+          </ul>
+        </div>
+      ) : (
+        <div className="empty-search">
+          <h4>No results found :(</h4>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
