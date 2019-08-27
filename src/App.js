b99edec5fada3c5321fd45f58156a7886d0c08a2
Henrique S. Hinke <hhinke@gmail.com>
2019-08-27 00:37:45 -0300

    Added code to fetch daily asset price and store formated data on state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 import './App.css';
+import ReportDispatcher from 'jest-jasmine2/build/jasmine/ReportDispatcher';
 
 export default function App() {
+  const API_KEY = '<YOUR_APIKEY_HERE>';
   const [bestMatches, setBestMatches] = useState(null);
+  const [currentChartData, setCurrentChartData] = useState(null);
 
   async function loadSearch(event) {
-    const API_KEY = '<YOUR_APIKEY_HERE>';
     const searchAsset = event.target.value;
     if(searchAsset.length < 4) {
       setBestMatches(null);
@@ -27,6 +29,50 @@ export default function App() {
     }
   }
 
+  async function loadChart(event) {
+    const assetName = event.currentTarget.dataset.id;
+
+    const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${assetName}&interval=30min&apikey=${API_KEY}`);
+    const { status, statusText } = response;
+    console.log(response);
+
+    if(status === 200 && statusText === 'OK') {
+      const keys = Object.keys(response.data);
+      if(keys.length === 2) {
+        const rawChartData = response.data[keys[1]];
+        const rawChartDataKeys = Object.keys(rawChartData);
+        if(rawChartDataKeys.length > 0) {
+          let chartData = [];
+          rawChartDataKeys.map(rawKey => {
+            chartData.push({
+              datetime: rawKey,
+              closing: rawChartData[rawKey]['4. close']
+            });
+          });
+
+          console.log('before', chartData);
+          const today = new Date();
+          const date  = today.getFullYear() + '-'
+             + ('0' + (today.getMonth()+1)).slice(-2) + '-' 
+             + ('0' + (today.getDate()-1)).slice(-2);
+          console.log(date);
+          chartData = chartData.filter(item => {
+            return item.datetime.includes(date);
+          });
+
+          setCurrentChartData(chartData);
+          console.log('after', chartData);
+        } else {
+          setCurrentChartData(null);
+        }
+      } else {
+        setCurrentChartData(null);
+      }
+    } else {
+      setCurrentChartData(null);
+    }
+  }
+
   return (
     <div className="main-container">
       <input id='assetName' 
@@ -38,7 +84,9 @@ export default function App() {
         <div className="results-container">
           <ul className="results-list">
             { bestMatches.map(match => (
-              <li key={match['1. symbol']}>
+              <li key={match['1. symbol']} 
+                  data-id={match['1. symbol']} 
+                  onClick={loadChart}>
                 <h3>{match['1. symbol']}</h3>
                 <p>{match['2. name']}</p>
               </li> 
